233700c81bbcdbabcad5951f199cfc5186c3c0d3
Ani <dzaficanida@gmail.com>
2025-01-13 22:33:59 +0100

    Add files via upload

---
+++ js/suprise.js
@@ -0,0 +1,8 @@
+document.querySelector('.button-suprise').addEventListener('click', function() {
+    Swal.fire({
+        title: '...',
+        text: 'Modified by Anida Džafić with text adjustments, custom icons, and a bezier SVG Instagram logo. Based on a template from FreeHTML5.',
+        icon: 'info',
+        confirmButtonText: 'Close'
+    });
+});
